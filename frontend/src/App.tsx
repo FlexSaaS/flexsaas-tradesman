f3bb93c2a74ffd49780f51873343f03d6d00a849
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2025-06-12 16:40:16 +0100

    fix merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-
 import { Route, Routes } from "react-router-dom";
 import ConfigTest from "./Components/ConfigTest";
 import ProjectPageTemp from "./pages/ProjectPageTemp";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage"; 
 import AboutPageTemp from "./pages/AboutPageTemp";
 import styled, { createGlobalStyle } from "styled-components";
 import { getClientConfig } from "./lib/getClientConfig";
-
 
 function App() {
   const client = getClientConfig();
 
   return (
-    <AppContainer fontFamily={client.fontFamily}>
+    <AppContainer fontFamily={client.fontFamily || "sans-serif"}>
       <GlobalStyle />
-    <Routes>
-      <Route path="/projects" element={<ProjectPageTemp />} />
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/about" element={<AboutPageTemp />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectPageTemp />} />
+        <Route path="/about" element={<AboutPageTemp />} />
+      </Routes>
     </AppContainer>
+  );
 }
 
 export default App;
@@ -28,18 +27,19 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     margin: 0;
     padding: 0;
+  }
 
-    * {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-    }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
 `;
 
 const AppContainer = styled.div<{ fontFamily: string }>`
   font-family: ${(props) => props.fontFamily}, sans-serif;
   min-height: 100vh;
+  display: flex;
   flex-direction: column;
   width: 100%;
   overflow-x: hidden;

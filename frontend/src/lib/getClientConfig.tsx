e14bf7dcfb41dfcc6d9400928d18534c3da70c7f
safo samson <safosamson18@gmail.com>
2026-03-01 12:41:08 +0000

    remove clientA and clientB

--- frontend/src/lib/getClientConfig.tsx
+++ frontend/src/lib/getClientConfig.tsx
@@ -1,23 +1,18 @@
 import type { ClientConfig } from "../types/Config";
-import clientAConfig from "../configs/clientA";
-import clientBConfig from "../configs/clientB";
+
 import clientBeakomConfig from "../configs/Beakom";
 import clientPrintifyConfig from "../configs/Printify";
 import LecienConfig from "../configs/Lecien";
 import EtherealVistaConfig from "../configs/Ethereal";
-// import other configs...
 
 const configMap: Record<string, ClientConfig> = {
-  A: clientAConfig,
-  B: clientBConfig,
   Beakom: clientBeakomConfig,
   Printify: clientPrintifyConfig,
   Lecien: LecienConfig,
   Ethereal: EtherealVistaConfig,
-  // ...
 };
 
 export function getClientConfig(): ClientConfig {
   const key = import.meta.env.VITE_CLIENT || "B";
-  return configMap[key] || clientAConfig;
+  return configMap[key] || LecienConfig;
 }
